427bb966e4b03969d3a85d0885d380915d586e32
nicola_tommasi <nicola.tommasi@comvation.com>
2017-04-11 14:51:05 +0200

    core Locale: Adds mouseleave event for locale dropdown

--- core/Locale/View/Script/LocaleDropdown.js
+++ core/Locale/View/Script/LocaleDropdown.js
@@ -1,5 +1,14 @@
 cx.jQuery(document).ready(function() {
   cx.jQuery(".language-icons.dropdown").click(function() {
     cx.jQuery(this).children(".language-icons-expanded").toggle();
+  }).live("mouseleave", function(event) {
+    if (!cx.jQuery(event.target).is('li.language-icon') &&
+      cx.jQuery('.language-icons-expanded').length > 0
+    ) {
+      cx.jQuery('.language-icons-expanded').each(function() {
+        cx.jQuery(this).parent().parent().children().css('z-index', 'auto');
+        cx.jQuery(this).hide();
+      });
+    }
   });
 });
