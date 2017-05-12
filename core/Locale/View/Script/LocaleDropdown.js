ad33604cd2e9269f81d2e34c8d7959a30dbd3903
nicola_tommasi <nicola.tommasi@comvation.com>
2017-05-12 16:32:19 +0200

    core ContentManager & Locale: Fixes annoying hiding behaviour of dropdowns

--- core/Locale/View/Script/LocaleDropdown.js
+++ core/Locale/View/Script/LocaleDropdown.js
@@ -1,14 +1,11 @@
 cx.jQuery(document).ready(function() {
-  cx.jQuery(".language-icons.dropdown").click(function() {
+  var languageIconsDropdown = cx.jQuery(".language-icons.dropdown");
+  cx.jQuery(document).click(function() {
+    cx.jQuery(".language-icons-expanded").hide();
+  });
+  languageIconsDropdown.click(function(e) {
+    e.stopPropagation();
     cx.jQuery(this).children(".language-icons-expanded").toggle();
-  }).live("mouseleave", function(event) {
-    if (!cx.jQuery(event.target).is('li.language-icon') &&
-      cx.jQuery('.language-icons-expanded').length > 0
-    ) {
-      cx.jQuery('.language-icons-expanded').each(function() {
-        cx.jQuery(this).parent().parent().children().css('z-index', 'auto');
-        cx.jQuery(this).hide();
-      });
-    }
+    languageIconsDropdown.not(this).children(".language-icons-expanded").hide();
   });
 });
