af13208cfe4ac6f66e07fe9fd32383a12257e459
Michael Ritter <michael.ritter@cloudrexx.com>
2017-08-17 11:46:32 +0200

    modules Feed (update): Compatibility with Locale

--- modules/Feed/Controller/EsiWidgetController.class.php
+++ modules/Feed/Controller/EsiWidgetController.class.php
@@ -76,12 +76,13 @@ class EsiWidgetController extends \Cx\Core_Modules\Widget\Controller\EsiWidgetCo
             return;
         }
 
+        $langId = \FWLanguage::getLangIdByIso639_1($params['locale']);
         $_CORELANG  = array_merge(
             $_CORELANG,
             \Env::get('init')->getComponentSpecificLanguageData(
                 'Core',
                 true,
-                $params['lang']
+                $langId
             )
         );
         $objNewsML  = new NewsML();
