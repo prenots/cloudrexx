d537e4c183d82081e24539709c14445dae74433f
Michael Ritter <michael.ritter@cloudrexx.com>
2017-11-16 16:04:19 +0100

    core_modules MultiSite (update): Move interface methods to correct location (see follow-ups for renaming)

--- core_modules/MultiSite/Controller/SubscriptionController.interface.php
+++ core_modules/MultiSite/Controller/SubscriptionController.interface.php
@@ -15,109 +15,11 @@ namespace Cx\Core_Modules\MultiSite\Controller;
 interface SubscriptionController {
 
     /**
-     * Creates a subscription
-     * 
-     * @param string  $domain
-     * @param integer $ipAddress
-     * @param integer $subscriptionStatus
-     * @param integer $customerId default null
-     * @param integer $planId default null
-     * 
-     * @return subcription id
-     */
-    public function createSubscription($domain, $ipAddress, $subscriptionStatus = 0, $customerId = null, $planId = null);
-    
-    /**
-     * Removes a subscription
-     * 
-     * @param int $subscriptionId
-     * 
-     * @throws MultiSiteDbException On error
-     */
-    public function removeSubscription($subscriptionId);
-        
-    /**
-     * Rename a subscription
-     * 
-     * @param string $domain domain name
-     * 
-     * @return subscription id
-     */
-    public function renameSubscriptionName($domain);
-    
-    /**
-     * Creates a user account
-     * 
-     * @param string  $domain
-     * @param string  $password
-     * @param string  $role
-     * @param integer $accountId
-     * 
-     * @return id
-     */
-    public function createUserAccount($domain, $password, $role, $accountId = null);
-    
-    /**
-     * Delete a user account
-     * 
-     * @param $userAccountId
-     * 
-     * @return id 
-     */
-    public function deleteUserAccount($userAccountId);
-    
-    /**
-     * Change the password from a user account
-     * 
-     * @param int $userAccountId user id
-     * @param string $password
-     * 
-     * @return id 
-     */
-    public function changeUserAccountPassword($userAccountId, $password);
-
-    /**
      * Creaye a Customer
      * @param \Cx\Core\Model\Model\Entity\Subscription
      * @throws MultiSiteDbException On error
      */
     public function createCustomer(\Cx\Core_Modules\MultiSite\Model\Entity\Customer $customer);
-
-    /**
-     * Create new domain alias
-     * @param string $aliasName alias name
-     */
-    public function createDomainAlias($aliasName);
-    
-    /**
-     * Rename the domain alias
-     * @param string $oldAliasName old alias name
-     * @param string $newAliasName new alias name
-     */
-    public function renameDomainAlias($oldAliasName, $newAliasName);
-    
-    /**
-     * Remove the domain alias by name
-     * @param string $aliasName alias name to delete
-     */
-    public function deleteDomainAlias($aliasName);
-    
-    /**
-     * Change the plan of the subscription
-     * 
-     * @param id     $subscriptionId  subcription id
-     * @param string $planGuid        planGuid
-     */
-    public function changePlanOfSubscription($subscriptionId, $planGuid);
-    
-    /**
-     * Create a new auto-login url for Panel.
-     * 
-     * @param integer $subscriptionId subscription id
-     * @param string  $ipAddress      ip address
-     * @param string  $sourceAddress  source address
-     */
-    public function getPanelAutoLoginUrl($subscriptionId, $ipAddress, $sourceAddress, $role);  
     
     /**
      * Get the all available service plans of mail service server
