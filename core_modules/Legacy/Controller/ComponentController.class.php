1b5106879beb7e217757e562153d4a7b05c64b7c
Michael Ritter <michael.ritter@cloudrexx.com>
2018-06-28 14:43:41 +0200

    refactor(legacy): return early & use NodePlaceholder class (CLX-2183)

--- core_modules/Legacy/Controller/ComponentController.class.php
+++ core_modules/Legacy/Controller/ComponentController.class.php
@@ -79,32 +79,37 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
         $eventArgs['content'] = preg_replace_callback(
             '/(?:{|\[\[)NODE_([A-Z0-9_]+)(?:}|\]\])/',
             function($matches) {
-                return 'func_node(' . str_replace('_', ',', $matches[1]) . ')';
+                $placeholder = \Cx\Core\Routing\NodePlaceholder::fromPlaceholder(
+                    $matches[0]
+                );
+                return $placeholder->getPlaceholder();
             },
             $eventArgs['content']
         );
-        if ($eventName == 'View.Sigma:setVariable') {
-            // parse widgets again as Sigma does not parse placeholders /
-            // callbacks on parse()
-            $template = new \Cx\Core_Modules\Widget\Model\Entity\Sigma();
-            $template->setTemplate($eventArgs['content']);
-            $targetComponent = '';
-            $targetEntity = '';
-            $targetId = '';
-            if ($template->getParseTarget()) {
-                $targetComponent = $template->getParseTarget()->getSystemComponent()->getName();
-                $targetEntity = get_class($template->getParseTarget());
-                $targetId = $template->getParseTarget()->getId();
-            }
-            $this->getComponent('Widget')->parseWidgets(
-                $template,
-                $targetComponent,
-                $targetEntity,
-                $targetId
-            );
-            $eventArgs['content'] = $template->get();
+        if ($eventName != 'View.Sigma:setVariable') {
+            // no need to parse widgets for View.Sigma:loadContent again as
+            // Sigma parses callback functions in content on parse().
+            return;
         }
-        // no need to parse widgets for View.Sigma:loadContent again as
-        // Sigma parses callback functions in content on parse().
+        // parse widgets again as Sigma does not parse placeholders /
+        // callbacks on parse()
+        $template = new \Cx\Core_Modules\Widget\Model\Entity\Sigma();
+        $template->setTemplate($eventArgs['content']);
+        $targetComponent = '';
+        $targetEntity = '';
+        $targetId = '';
+        if ($eventArgs['template']->getParseTarget()) {
+            $targetComponent = $eventArgs['template']->getParseTarget(
+            )->getSystemComponent()->getName();
+            $targetEntity = get_class($eventArgs['template']->getParseTarget());
+            $targetId = $eventArgs['template']->getParseTarget()->getId();
+        }
+        $this->getComponent('Widget')->parseWidgets(
+            $template,
+            $targetComponent,
+            $targetEntity,
+            $targetId
+        );
+        $eventArgs['content'] = $template->get();
     }
 }
