02159db7580578c2ed936da4a2de5552484f6cf0
Sam Hawkes <sam.hawkes@comvation.com>
2018-09-18 15:33:13 +0200

    docs(shop): add doc blocks to entity classes (CLX-2260)

--- modules/Shop/Model/Entity/Attribute.class.php
+++ modules/Shop/Model/Entity/Attribute.class.php
@@ -1,9 +1,60 @@
 <?php
 
+/**
+ * Cloudrexx
+ *
+ * @link      http://www.cloudrexx.com
+ * @copyright Cloudrexx AG 2007-2018
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Cloudrexx" is a registered trademark of Cloudrexx AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+/**
+ * Class Attribute
+ *
+ * @copyright   CLOUDREXX CMS - Cloudrexx AG Thun
+ * @author      Sam Hawkes <info@cloudrexx.com>
+ * @package     cloudrexx
+ * @subpackage  module_shop
+ * @version     5.0.0
+ */
+
 namespace Cx\Modules\Shop\Model\Entity;
 
 /**
  * Attribute
+ *
+ * These may be associated with zero or more Products.
+ * Each attribute consists of a name part
+ * (module_shop_attribute) and zero or more value parts (module_shop_option).
+ * Each of the values can be associated with an arbitrary number of Products
+ * by inserting the respective record into the relations table
+ * module_shop_products_attributes.
+ * The type determines the kind of relation between a Product and the attribute
+ * values, that is, whether it is optional or mandatory, and whether single
+ * or multiple attributes may be chosen at a time.  See {@link ?} for details.
+ *
+ * @copyright   CLOUDREXX CMS - Cloudrexx AG Thun
+ * @author      Sam Hawkes <info@cloudrexx.com>
+ * @package     cloudrexx
+ * @subpackage  module_shop
+ * @version     5.0.0
  */
 class Attribute extends \Cx\Model\Base\EntityBase {
     /**
