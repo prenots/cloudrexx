87ca43c302a348b20329965736c63261d3bcd5b1
Michael Ritter <michael.ritter@cloudrexx.com>
2018-10-02 14:26:28 +0200

    feat(update): add missing DB structure for DataAccess (CLX-724)

--- update/updates/5.0.0/components/core_module/dataaccess.php
+++ update/updates/5.0.0/components/core_module/dataaccess.php
@@ -27,5 +27,88 @@
 
 function _dataaccessUpdate()
 {
+    global $objUpdate, $_CONFIG;
+
+    if ($objUpdate->_isNewerVersion($_CONFIG['coreCmsVersion'], '5.0.0')) {
+        try {
+            \Cx\Lib\UpdateUtil::table(
+                DBPREFIX.'core_module_data_access',
+                array(
+                    'id' => array('type' => 'INT(11)', 'notnull' => true, 'auto_increment' => true, 'primary' => true),
+                    'read_permission' => array('type' => 'INT(11)', 'notnull' => false, 'after' => 'id'),
+                    'write_permission' => array('type' => 'INT(11)', 'notnull' => false, 'after' => 'read_permission'),
+                    'data_source_id' => array('type' => 'INT(11)', 'notnull' => false, 'after' => 'write_permission'),
+                    'name' => array('type' => 'VARCHAR(255)', 'after' => 'data_source_id'),
+                    'field_list' => array('type' => 'longtext', 'after' => 'name'),
+                    'access_condition' => array('type' => 'longtext', 'after' => 'field_list'),
+                    'allowed_output_methods' => array('type' => 'longtext', 'after' => 'access_condition'),
+                ),
+                array(
+                    'name' => array('fields' => array('name'), 'type' => 'UNIQUE'),
+                ),
+                'InnoDB',
+                '',
+                array(
+                    'read_permission' => array(
+                        'table' => DBPREFIX.'core_modules_access_permission',
+                        'column' => 'id',
+                        'onDelete' => 'NO ACTION',
+                        'onUpdate' => 'NO ACTION',
+                    ),
+                    'write_permission' => array(
+                        'table' => DBPREFIX.'core_modules_access_permission',
+                        'column' => 'id',
+                        'onDelete' => 'NO ACTION',
+                        'onUpdate' => 'NO ACTION',
+                    ),
+                    'data_source_id' => array(
+                        'table' => DBPREFIX.'core_data_source',
+                        'column' => 'id',
+                        'onDelete' => 'NO ACTION',
+                        'onUpdate' => 'NO ACTION',
+                    ),
+                )
+            );
+            \Cx\Lib\UpdateUtil::table(
+                DBPREFIX.'core_module_data_access_apikey',
+                array(
+                    'id' => array('type' => 'INT(11)', 'notnull' => true, 'auto_increment' => true, 'primary' => true),
+                    'api_key' => array('type' => 'VARCHAR(32)', 'after' => 'id')
+                ),
+                array(
+                    'api_key' => array('fields' => array('api_key'), 'type' => 'UNIQUE')
+                )
+            );
+            \Cx\Lib\UpdateUtil::table(
+                DBPREFIX.'core_module_data_access_data_access_apikey',
+                array(
+                    'id' => array('type' => 'INT(11)', 'notnull' => true, 'auto_increment' => true, 'primary' => true),
+                    'api_key_id' => array('type' => 'INT(11)', 'notnull' => false, 'after' => 'id'),
+                    'data_access_id' => array('type' => 'INT(11)', 'notnull' => false, 'after' => 'api_key_id'),
+                    'read_only' => array('type' => 'TINYINT(1)', 'notnull' => false, 'after' => 'data_access_id'),
+                ),
+                array(),
+                'InnoDB',
+                '',
+                array(
+                    'api_key_id' => array(
+                        'table' => DBPREFIX.'core_module_data_access_apikey',
+                        'column' => 'id',
+                        'onDelete' => 'NO ACTION',
+                        'onUpdate' => 'NO ACTION',
+                    ),
+                    'data_access_id' => array(
+                        'table' => DBPREFIX.'core_module_data_access',
+                        'column' => 'id',
+                        'onDelete' => 'NO ACTION',
+                        'onUpdate' => 'NO ACTION',
+                    ),
+                )
+            );
+        } catch (\Cx\Lib\UpdateException $e) {
+            return \Cx\Lib\UpdateUtil::DefaultActionHandler($e);
+        }
+    }
+
     return true;
 }
