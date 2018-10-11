af7336bf4f17169ca237e4bc88516fd3dbf6ae42
Sam Hawkes <sam.hawkes@comvation.com>
2018-10-11 14:36:24 +0200

    fix(shop): migrate data (CLX-2260)

--- modules/Shop/Data/Migration.sql
+++ modules/Shop/Data/Migration.sql
@@ -6,12 +6,19 @@ CREATE TABLE contrexx_module_shop_rel_category_pricelist (
     INDEX IDX_B56E91A189045958 (pricelist_id),
     PRIMARY KEY(category_id, pricelist_id)
   ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
 CREATE TABLE contrexx_module_shop_rel_category_product (
     category_id INT UNSIGNED NOT NULL,
     product_id INT UNSIGNED NOT NULL,
     INDEX IDX_DA4CA51112469DE2 (category_id),
     INDEX IDX_DA4CA5114584665A (product_id),
     PRIMARY KEY(category_id, product_id)
+  ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE contrexx_module_shop_rel_product_user_group (
+    product_id INT UNSIGNED NOT NULL,
+    usergroup_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY(product_id, usergroup_id)
   ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
 
 
@@ -34,12 +41,11 @@ ALTER TABLE contrexx_module_shop_option
 
 ALTER TABLE contrexx_module_shop_products
   ADD uri VARCHAR(55) DEFAULT '' NOT NULL,
-  ADD short LONGTEXT DEFAULT '' NOT NULL,
-  ADD `long` LONGTEXT DEFAULT '' NOT NULL,
+  ADD short LONGTEXT NOT NULL,
+  ADD `long` LONGTEXT NOT NULL,
   ADD name VARCHAR(45) DEFAULT '' NOT NULL,
-  ADD `keys` LONGTEXT DEFAULT '' NOT NULL,
+  ADD `keys` LONGTEXT NOT NULL,
   ADD code VARCHAR(45) DEFAULT '' NOT NULL,
-  DROP category_id,
   CHANGE date_start date_start DATETIME DEFAULT '0000-00-00 00:00:00' NOT NULL,
   CHANGE date_end date_end DATETIME DEFAULT '0000-00-00 00:00:00' NOT NULL;
 
@@ -47,20 +53,15 @@ ALTER TABLE contrexx_module_shop_payment ADD name VARCHAR(45) DEFAULT '' NOT NUL
 
 ALTER TABLE contrexx_module_shop_shipper ADD name VARCHAR(30) DEFAULT '' NOT NULL;
 
+ALTER TABLE contrexx_module_shop_vat ADD class VARCHAR(35) DEFAULT '' NOT NULL;
+
 ALTER TABLE contrexx_module_shop_article_group ADD name VARCHAR(45) DEFAULT '' NOT NULL;
 
 ALTER TABLE contrexx_module_shop_categories
   ADD name VARCHAR(45) DEFAULT '' NOT NULL,
-  ADD description LONGTEXT DEFAULT '' NOT NULL;
+  ADD description LONGTEXT NOT NULL;
 
-ALTER TABLE contrexx_module_shop_discount_coupon
-  CHANGE customer_id customer_id INT NOT NULL,
-  CHANGE payment_id payment_id INT UNSIGNED DEFAULT NULL,
-  CHANGE product_id product_id INT UNSIGNED DEFAULT NULL;
-
-ALTER TABLE contrexx_module_shop_pricelists
-  DROP categories,
-  CHANGE lang_id lang_id INT DEFAULT 0 NOT NULL;
+ALTER TABLE contrexx_module_shop_pricelists CHANGE lang_id lang_id INT DEFAULT 0 NOT NULL;
 
 ALTER TABLE contrexx_module_shop_rel_customer_coupon CHANGE customer_id customer_id INT DEFAULT 0 NOT NULL;
 
@@ -79,18 +80,30 @@ ALTER TABLE contrexx_module_shop_rel_shipper
   CHANGE shipper_id shipper_id INT UNSIGNED NOT NULL,
   CHANGE zone_id zone_id INT UNSIGNED NOT NULL;
 
+ALTER TABLE contrexx_module_shop_customer_group ADD name VARCHAR(45) DEFAULT '' NOT NULL;
 
-/** Damit die Beziehungen ohne Probleme eingefügt werden können· **/
-UPDATE `contrexx_module_shop_products` SET `article_id`=NULL WHERE `article_id`= 0;
-UPDATE `contrexx_module_shop_products` SET `group_id`=NULL WHERE `group_id`= 0;
-
-UPDATE `contrexx_module_shop_discount_coupon` SET `payment_id`=NULL WHERE `payment_id`= 0;
-UPDATE `contrexx_module_shop_discount_coupon` SET `product_id`=NULL WHERE `product_id`= 0;
+ALTER TABLE contrexx_module_shop_categories CHANGE description description LONGTEXT DEFAULT '' NOT NULL;
 
 
 /** Drop Primary Keys **/
 ALTER TABLE contrexx_module_shop_rel_countries DROP PRIMARY KEY;
+ALTER TABLE contrexx_module_shop_discount_coupon DROP PRIMARY KEY;
 
+
+/** Damit die Beziehungen ohne Probleme eingefügt werden können· **/
+ALTER TABLE contrexx_module_shop_discount_coupon
+  CHANGE customer_id customer_id INT NULL,
+  CHANGE payment_id payment_id INT UNSIGNED DEFAULT NULL,
+  CHANGE product_id product_id INT UNSIGNED DEFAULT NULL;
+UPDATE `contrexx_module_shop_discount_coupon` SET `payment_id`=NULL WHERE `payment_id`= 0;
+UPDATE `contrexx_module_shop_discount_coupon` SET `product_id`=NULL WHERE `product_id`= 0;
+UPDATE `contrexx_module_shop_discount_coupon` SET `customer_id`=NULL WHERE `customer_id`= 0;
+
+UPDATE `contrexx_module_shop_products` SET `article_id`=NULL WHERE `article_id`= 0;
+UPDATE `contrexx_module_shop_products` SET `group_id`=NULL WHERE `group_id`= 0;
+
+ALTER TABLE contrexx_module_shop_categories CHANGE parent_id parent_id INT UNSIGNED DEFAULT NULL;
+UPDATE `contrexx_module_shop_categories` SET `parent_id`=NULL WHERE `parent_id`= 0;
 
 /** Constraints **/
 ALTER TABLE contrexx_module_shop_rel_category_pricelist ADD CONSTRAINT FK_B56E91A112469DE2 FOREIGN KEY (category_id) REFERENCES contrexx_module_shop_categories (id);
@@ -125,6 +138,7 @@ ALTER TABLE contrexx_module_shop_order_items ADD CONSTRAINT FK_1D79476B4584665A 
 
 ALTER TABLE contrexx_module_shop_discount_coupon ADD CONSTRAINT FK_7E70AB1A4C3A3BB FOREIGN KEY (payment_id) REFERENCES contrexx_module_shop_payment (id);
 ALTER TABLE contrexx_module_shop_discount_coupon ADD CONSTRAINT FK_7E70AB1A4584665A FOREIGN KEY (product_id) REFERENCES contrexx_module_shop_products (id);
+ALTER TABLE contrexx_module_shop_discount_coupon ADD CONSTRAINT FK_7E70AB1A9395C3F3 FOREIGN KEY (customer_id) REFERENCES contrexx_access_users (id);
 
 ALTER TABLE contrexx_module_shop_discountgroup_count_rate ADD CONSTRAINT FK_3F3DD477FE54D947 FOREIGN KEY (group_id)
   REFERENCES contrexx_module_shop_discountgroup_count_name (id);
@@ -147,6 +161,7 @@ ALTER TABLE contrexx_module_shop_rel_shipper ADD CONSTRAINT FK_87E5C96838459F23 
 
 ALTER TABLE contrexx_module_shop_shipment_cost ADD CONSTRAINT FK_2329A4538459F23 FOREIGN KEY (shipper_id) REFERENCES contrexx_module_shop_shipper (id);
 
+ALTER TABLE contrexx_module_shop_categories ADD CONSTRAINT FK_A9242624727ACA70 FOREIGN KEY (parent_id) REFERENCES contrexx_module_shop_categories (id);
 
 /** Indexe **/
 CREATE INDEX IDX_DA286BB1B213FA4 ON contrexx_module_shop_orders (lang_id);
@@ -192,8 +207,181 @@ CREATE INDEX IDX_87E5C96838459F23 ON contrexx_module_shop_rel_shipper (shipper_i
 
 CREATE INDEX IDX_2329A4538459F23 ON contrexx_module_shop_shipment_cost (shipper_id);
 
+CREATE INDEX IDX_A9242624727ACA70 ON contrexx_module_shop_categories (parent_id);
+
 
 /** Add Primary Keys **/
 ALTER TABLE contrexx_module_shop_rel_countries ADD PRIMARY KEY (zone_id, country_id);
 
 ALTER TABLE contrexx_module_shop_rel_shipper ADD PRIMARY KEY (zone_id, shipper_id);
+
+/** customer_id kann kein primary key sein da es auch 0 Werte gibt. **/
+ALTER TABLE contrexx_module_shop_discount_coupon ADD PRIMARY KEY (code);
+
+
+/** Merge Data **/
+INSERT INTO contrexx_module_shop_rel_category_product SELECT
+    c.id AS category_id, d.id AS product_id
+FROM
+    contrexx_module_shop_categories c
+    JOIN contrexx_module_shop_products d
+        ON d.category_id REGEXP CONCAT('[[:<:]]', c.id, '[[:>:]]');
+
+INSERT INTO contrexx_module_shop_rel_category_pricelist SELECT
+    c.id AS category_id, d.id AS pricelist_id
+FROM
+    contrexx_module_shop_categories c
+    JOIN contrexx_module_shop_pricelists d
+        ON d.categories REGEXP CONCAT('[[:<:]]', c.id, '[[:>:]]') OR d.categories = '*';
+
+INSERT INTO contrexx_module_shop_rel_product_user_group SELECT
+    c.group_id AS usergroup_id, d.id AS product_id
+FROM
+    contrexx_access_user_groups c
+    JOIN contrexx_module_shop_products d
+        ON d.usergroup_ids REGEXP CONCAT('[[:<:]]', c.group_id, '[[:>:]]');
+
+/** Zones **/
+UPDATE contrexx_module_shop_zones AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'zone_name' AND t.lang_id = 1;
+
+/** Shipper **/
+UPDATE contrexx_module_shop_shipper AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'shipper_name' AND t.lang_id = 1;
+
+/** Products **/
+UPDATE contrexx_module_shop_products AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.uri = t.text
+  WHERE t.section = 'Shop' AND t.key = 'product_uri' AND t.lang_id = 1;
+
+UPDATE contrexx_module_shop_products AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.short = t.text
+  WHERE t.section = 'Shop' AND t.key = 'product_short' AND t.lang_id = 1;
+
+UPDATE contrexx_module_shop_products AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.long = t.text
+  WHERE t.section = 'Shop' AND t.key = 'product_long' AND t.lang_id = 1;
+
+UPDATE contrexx_module_shop_products AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'product_name' AND t.lang_id = 1;
+
+UPDATE contrexx_module_shop_products AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.keys = t.text
+  WHERE t.section = 'Shop' AND t.key = 'product_keys' AND t.lang_id = 1;
+
+UPDATE contrexx_module_shop_products AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.code = t.text
+  WHERE t.section = 'Shop' AND t.key = 'product_code' AND t.lang_id = 1;
+
+/** Attribute **/
+UPDATE contrexx_module_shop_attribute AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'attribute_name' AND t.lang_id = 1;
+
+/** Option **/
+UPDATE contrexx_module_shop_option AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'option_name' AND t.lang_id = 1;
+
+/** Category **/
+UPDATE contrexx_module_shop_categories AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.description = t.text
+  WHERE t.section = 'Shop' AND t.key = 'category_description' AND t.lang_id = 1;
+
+UPDATE contrexx_module_shop_categories AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'category_name' AND t.lang_id = 1;
+
+/** Manufacturer **/
+UPDATE contrexx_module_shop_manufacturer AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.uri = t.text
+  WHERE t.section = 'Shop' AND t.key = 'manufacturer_uri' AND t.lang_id = 1;
+
+UPDATE contrexx_module_shop_manufacturer AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'manufacturer_name' AND t.lang_id = 1;
+
+/** Payment **/
+UPDATE contrexx_module_shop_payment AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'payment_name' AND t.lang_id = 1;
+
+/** Currency **/
+UPDATE contrexx_module_shop_currencies AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'currency_name' AND t.lang_id = 1;
+
+/** Vat **/
+UPDATE contrexx_module_shop_vat AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.class = t.text
+  WHERE t.section = 'Shop' AND t.key = 'vat_class' AND t.lang_id = 1;
+
+/** Discountgroup Count Name **/
+UPDATE contrexx_module_shop_discountgroup_count_name AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.unit = t.text
+  WHERE t.section = 'Shop' AND t.key = 'discount_group_unit' AND t.lang_id = 1;
+UPDATE contrexx_module_shop_discountgroup_count_name AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'discount_group_name' AND t.lang_id = 1;
+
+/** Article Group **/
+UPDATE contrexx_module_shop_article_group AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'discount_group_article' AND t.lang_id = 1;
+
+/** Customer Group **/
+UPDATE contrexx_module_shop_customer_group AS z
+	INNER JOIN contrexx_core_text AS t ON z.id = t.id
+  SET z.name = t.text
+  WHERE t.section = 'Shop' AND t.key = 'discount_group_customer' AND t.lang_id = 1;
+
+
+/** Drop **/
+ALTER TABLE contrexx_module_shop_pricelists DROP categories;
+ALTER TABLE contrexx_module_shop_products DROP category_id;
+ALTER TABLE contrexx_module_shop_products DROP usergroup_ids;
+
+DELETE FROM `contrexx_core_text` WHERE `key` = 'zone_name';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'shipper_name';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'product_uri';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'product_short';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'product_long';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'product_name';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'product_keys';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'product_code';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'attribute_name';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'option_name';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'category_description';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'category_name';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'manufacturer_uri';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'manufacturer_name';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'payment_name';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'currency_name';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'vat_class';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'discount_group_unit';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'discount_group_name';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'discount_group_article';
+DELETE FROM `contrexx_core_text` WHERE `key` = 'discount_group_customer';
