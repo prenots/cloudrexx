cdc3ed4dc382ade6f35c58a5d232647e38a9b995
Sam Hawkes <sam.hawkes@comvation.com>
2018-11-02 08:34:11 +0100

    fix(shop): use correct variable (CLX-2260)

--- modules/Shop/Model/Entity/Orders.class.php
+++ modules/Shop/Model/Entity/Orders.class.php
@@ -1090,7 +1090,7 @@ class Orders extends \Cx\Model\Base\EntityBase {
      */
     public function setLang(\Cx\Core\Locale\Model\Entity\Locale $lang = null)
     {
-        $this->currencies = $lang;
+        $this->lang = $lang;
     }
 
     /**
