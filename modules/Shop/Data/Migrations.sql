3c977ef9c1286208d10aad45b298c9a33cff425c
Sam Hawkes <sam.hawkes@comvation.com>
2018-11-15 09:00:48 +0100

    chore(shop): update shop tables in db (CLX-2422)

---
+++ modules/Shop/Data/Migrations.sql
@@ -0,0 +1,42 @@
+/** Create Tables **/
+CREATE TABLE contrexx_module_shop_rel_category_pricelist (
+  category_id INT UNSIGNED NOT NULL,
+  pricelist_id INT UNSIGNED NOT NULL,
+  PRIMARY KEY(category_id, pricelist_id)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE contrexx_module_shop_rel_category_product (
+  category_id INT UNSIGNED NOT NULL,
+  product_id INT UNSIGNED NOT NULL,
+  PRIMARY KEY(category_id, product_id)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE contrexx_module_shop_rel_product_user_group (
+  product_id INT UNSIGNED NOT NULL,
+  usergroup_id INT NOT NULL,
+  PRIMARY KEY(product_id, usergroup_id)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+/** Merge Data **/
+INSERT INTO contrexx_module_shop_rel_category_product SELECT
+    c.id AS category_id, d.id AS product_id
+FROM
+    contrexx_module_shop_categories c
+    JOIN contrexx_module_shop_products d
+        ON d.category_id REGEXP CONCAT('[[:<:]]', c.id, '[[:>:]]');
+
+INSERT INTO contrexx_module_shop_rel_category_pricelist SELECT
+    c.id AS category_id, d.id AS pricelist_id
+FROM
+    contrexx_module_shop_categories c
+    JOIN contrexx_module_shop_pricelists d
+        ON d.categories REGEXP CONCAT('[[:<:]]', c.id, '[[:>:]]') OR d.categories = '*';
+
+INSERT INTO contrexx_module_shop_rel_product_user_group SELECT
+    c.group_id AS usergroup_id, d.id AS product_id
+FROM
+    contrexx_access_user_groups c
+    JOIN contrexx_module_shop_products d
+        ON d.usergroup_ids REGEXP CONCAT('[[:<:]]', c.group_id, '[[:>:]]');
+
+ALTER TABLE contrexx_module_shop_products DROP category_id;
