9131b191e67777e0ce35132bdf8c245565dae235
Sam Hawkes <sam.hawkes@comvation.com>
2018-11-20 10:25:40 +0100

    fix(shop): rename code() to setCode() (CLX-2421)

--- modules/Shop/Model/Entity/Products.class.php
+++ modules/Shop/Model/Entity/Products.class.php
@@ -829,7 +829,7 @@ class Products extends \Cx\Model\Base\EntityBase implements \Gedmo\Translatable\
      *
      * @param string $code
      */
-    public function code($code)
+    public function setCode($code)
     {
         $this->code = $code;
     }
