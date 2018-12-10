0d45ca0946fddd2187cfe44edd53959cb74d904a
Sam Hawkes <sam.hawkes@comvation.com>
2018-12-10 15:24:16 +0100

    fix(shop): change scope (CLX-2282)

--- modules/Shop/View/Script/Backend.js
+++ modules/Shop/View/Script/Backend.js
@@ -1,10 +1,10 @@
-var scope = 'manufacturer';
+var scope = 'shopDelete';
 cx.bind("delete", function (deleteIds) {
     if (confirm(
-        cx.variables.get('TXT_CONFIRM_DELETE_MANUFACTURER', scope)+'\n'+ cx.variables.get('TXT_ACTION_IS_IRREVERSIBLE', scope)
+        cx.variables.get('TXT_CONFIRM_DELETE', scope)+'\n'+ cx.variables.get('TXT_ACTION_IS_IRREVERSIBLE', scope)
     )) {
         window.location.replace(
             "?deleteids=" + encodeURI(deleteIds) + "&csrf=" + cx.variables.get('CSRF_PARAM', scope) + "&vg_increment_number=0"
         );
     }
-}, 'manufacturer');
+}, 'shopDelete');
