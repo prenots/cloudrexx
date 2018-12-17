0d5d2e4eabc8c9a65d3c994321e591d62ee9b638
Sam Hawkes <sam.hawkes@comvation.com>
2018-12-17 13:21:26 +0100

    refactor(shop): add pricelist controller (CLX-2283)

--- modules/Shop/Controller/ComponentController.class.php
+++ modules/Shop/Controller/ComponentController.class.php
@@ -52,7 +52,7 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
      */
     public function getControllerClasses()
     {
-        return array('Backend', 'Pdf');
+        return array('Backend', 'Pdf', 'Pricelist');
     }
 
     /**
