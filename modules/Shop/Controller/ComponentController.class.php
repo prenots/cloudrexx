963ca9faa5078e68d1d3e95aab9a35c77e338c17
Sam Hawkes <sam.hawkes@comvation.com>
2018-12-28 08:16:28 +0100

    fix(shop): add category controller to controller classes (CLX-2282)

--- modules/Shop/Controller/ComponentController.class.php
+++ modules/Shop/Controller/ComponentController.class.php
@@ -52,7 +52,7 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
      */
     public function getControllerClasses()
     {
-        return array('Backend', 'Manufacturer');
+        return array('Backend', 'Manufacturer', 'Category');
     }
 
     /**
