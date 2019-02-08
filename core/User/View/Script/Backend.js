f4c73d89f3af70b8b26934a22517d797c8ca3eeb
Dario Graf <dg@comvation.com>
2019-02-08 15:11:54 +0100

    refactor(user): add functions to get value for dropdown (CLX-2254)

--- core/User/View/Script/Backend.js
+++ core/User/View/Script/Backend.js
@@ -4,3 +4,17 @@
  * \JS::registerJS(substr($this->getDirectory(false, true) . '/View/Script/FileName.css', 1));
  * or remove this file if you don't need it
  */
+
+jQuery(document).ready(function($){
+    if ( $( "#form-0-emailAccess" ).length ) {
+        var $value = $( "#form-0-emailAccess" ).val();
+
+        $( '[name="emailAccess"]' ).val($value);
+    }
+
+    if ( $( "#form-0-backendLangId" ).length ) {
+        var $value = $( "#form-0-backendLangId" ).val();
+
+        $( '[name="backendLangId"]' ).val($value);
+    }
+});
