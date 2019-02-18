eced402ae12eecc62276fa6d42add8e150b51739
Sam Hawkes <sam.hawkes@comvation.com>
2019-02-18 08:22:39 +0100

    docs(shop): add class blocks (CLX-2281)

--- modules/Shop/Controller/OrderController.class.php
+++ modules/Shop/Controller/OrderController.class.php
@@ -1,16 +1,56 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: sam
- * Date: 03.01.19
- * Time: 12:59
+ * Cloudrexx
+ *
+ * @link      http://www.cloudrexx.com
+ * @copyright Cloudrexx AG 2007-2018
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Cloudrexx" is a registered trademark of Cloudrexx AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
  */
 
+/**
+ * OrderController to handle orders
+ *
+ * @copyright   Cloudrexx AG
+ * @author      Sam Hawkes <info@cloudrexx.com>
+ * @package     cloudrexx
+ * @subpackage  module_shop
+ */
 namespace Cx\Modules\Shop\Controller;
 
-
+/**
+ * OrderController to handle orders
+ *
+ * @copyright   Cloudrexx AG
+ * @author      Sam Hawkes <info@cloudrexx.com>
+ * @package     cloudrexx
+ * @subpackage  module_shop
+ */
 class OrderController extends \Cx\Core\Core\Model\Entity\Controller
 {
+    /**
+     * Get ViewGenerator options for Manufacturer entity
+     *
+     * @param $options array predefined ViewGenerator options
+     * @throws \Exception
+     * @return array includes ViewGenerator options for Manufacturer entity
+     */
     public function getViewGeneratorOptions($options)
     {
         global $_ARRAYLANG;
