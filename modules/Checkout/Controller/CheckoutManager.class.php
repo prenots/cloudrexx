2c86ee5a2911d808f15038fdb22b694782da1ac1
DeepakRajanM <deepak@softsolutions4u.info>
2019-05-15 13:07:47 +0530

    fix(checkout): use \Cx\Core\Routing\Url object method to redirect (CLX-2720)

--- modules/Checkout/Controller/CheckoutManager.class.php
+++ modules/Checkout/Controller/CheckoutManager.class.php
@@ -275,7 +275,7 @@ class CheckoutManager extends CheckoutLibrary {
         global $_ARRAYLANG;
 
         if (empty($_GET['id'])) {
-            \Cx\Core\Csrf\Controller\Csrf::redirect('index.php?cmd=Checkout');
+            \Cx\Core\Csrf\Controller\Csrf::redirect(\Cx\Core\Routing\Url::fromBackend('Checkout'));
         }
 
         $tableRow = '';
@@ -366,7 +366,7 @@ class CheckoutManager extends CheckoutLibrary {
         global $_ARRAYLANG;
 
         if (empty($_GET['id'])) {
-            \Cx\Core\Csrf\Controller\Csrf::redirect('index.php?cmd=Checkout');
+            \Cx\Core\Csrf\Controller\Csrf::redirect(\Cx\Core\Routing\Url::fromBackend('Checkout'));
         }
 
         if ($this->objTransaction->delete($_GET['id'])) {
